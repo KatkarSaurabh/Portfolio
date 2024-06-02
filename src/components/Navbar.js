87d5fdffcfa1e30eec90b646f442f977158b55d7
KatkarSaurabh <katkarsaurabh2@gmail.com>
2024-06-02 22:11:10 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,36 +3,29 @@ import './Navbar.css'
 import {Link,Outlet} from 'react-router-dom'
 import logo from '../assets/pfheader.jpg'
 import contact_logo from '../assets/contactmeImage.jpg'
-import {AppBar, Toolbar, Typography, IconButton, Stack} from '@mui/material'
+import {AppBar, Toolbar, Typography, IconButton, Stack, Box, Menu, MenuList, MenuItem} from '@mui/material'
 import {HomeRounded} from '@mui/icons-material';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
-  // return (
-  //   <nav className="navbar">
-  //     <div><Link to='/intro' className='logo'>SSK</Link></div>
-  //     <div className="menu">
-  //       <Link to='/intro' className='menuItem' >Home</Link>
-  //       <Link to='/about' className='menuItem'>About</Link>
-  //       <Link to='/projects' className='menuItem'>Projects</Link>
-  //     </div>
-  //       <Link className='contact' to='/contact'>ContactMe</Link>
-  //   </nav>
-  // );
-  return <AppBar position='fixed'>
-    <Toolbar>
-    <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+
+  return <AppBar position='static' style={{height:'3.5rem'}}>
+    <Toolbar style={{fontSize:'1rem'}}>
+    <IconButton size='large' edge='start' color='inherit' aria-label='logo' >
     <AcUnitIcon></AcUnitIcon>
     </IconButton>
-    <Typography style={{fontSize:'large'}} color='inherit' component='div' sx={{flexGrow:55}}>
+    <Typography color='inherit' component='div' sx={{flexGrow:1}}>
       Welcome
     </Typography>
-    <Stack style={{display:'flex',fontSize:'large'}} direction='row' spacing={4}>
+    <Box direction='row' spacing={4}>
       <Link to='/intro' className='menuItem'>Home</Link>
       <Link to='/about' className='menuItem'>About Me</Link>
       <Link to='/projects' className='menuItem'>Projects</Link>
-      <Link to='/contact' className='menuItem'>ContactMe</Link>
-    </Stack>
+      <Link to='/contact' className='menuItem'>Contact Me</Link>
+    </Box>
+    
+
     </Toolbar>
   </AppBar>
 }
